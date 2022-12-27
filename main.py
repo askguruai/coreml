1a3a369c254d3bc738d7f302f59607dca4118dab
Alexander Smirnov <furiousteabag@gmail.com>
2022-12-27 00:26:51 -0500

    add completions endpoint

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from utils import CONFIG
-from utils.api import EmbeddingsInput
+from utils.api import CompletionsInput, EmbeddingsInput
 from utils.logging import run_uvicorn_loguru
 
 app = FastAPI()
@@ -23,9 +23,37 @@ async def get_embeddings(embeddings_input: EmbeddingsInput):
     logging.info("text(s) to embed:" + '\n' + pformat(embeddings_input.input))
     return {
         "data": openai.Embedding.create(
-            input=embeddings_input.input, model=CONFIG["model"]["embeddings_model"]
+            input=embeddings_input.input, model=CONFIG["embeddings"]["model"]
         )["data"]
     }
+
+
+@app.post("/completions/")
+async def get_completions(completions_input: CompletionsInput):
+    prompt = (
+        CONFIG["completions"]["intro"]
+        + "\n\n"
+        + completions_input.info
+        + "\n\n"
+        + "Question: "
+        + completions_input.query
+        + '\n'
+        + "Answer:"
+    )
+    logging.info("completions request:" + '\n' + prompt)
+    answer = openai.Completion.create(
+        model=CONFIG["completions"]["model"],
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=300,
+        top_p=1,
+        frequency_penalty=0.0,
+        # frequency_penalty=15,
+        presence_penalty=0.6
+        # stop=['\n']
+    )
+    logging.info("completions result:" + '\n' + pformat(answer))
+    return {"data": answer["choices"][0]["text"]}
 
 
 if __name__ == "__main__":

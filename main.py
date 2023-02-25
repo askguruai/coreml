4e93a435bf0506222ae0cd00b8d85abeee3e1af8
Alexander Smirnov <furiousteabag@gmail.com>
2023-02-25 21:49:12 +0700

    add custom model to embeddings route

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import openai
 import uvicorn
 from fastapi import FastAPI, HTTPException, status
 
-from data import PROMPT_GENERAL, PROMPT_NO_INFO
+from data import EMBEDDING_INSTRUCTION, PROMPT_GENERAL, PROMPT_NO_INFO
+from ml import CompletionModel, EmbeddingModel
 from utils import CONFIG
-from utils.api import (
-    CompletionsInput,
-    CompletionsResponse,
-    EmbeddingsInput,
-    EmbeddingsResponse,
-    HTTPExceptionResponse,
-)
+from utils.api import (CompletionsInput, CompletionsResponse, EmbeddingsInput,
+                       EmbeddingsResponse, HTTPExceptionResponse)
 from utils.logging import run_uvicorn_loguru
+
+embedding_model = EmbeddingModel(
+    model_name=CONFIG["embeddings"]["model"], device=CONFIG["embeddings"]["device"]
+)
+completion_model = CompletionModel()
 
 app = FastAPI()
 
@@ -31,19 +32,20 @@ def read_root():
     responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
 )
 async def get_embeddings(embeddings_input: EmbeddingsInput):
-    logging.info(
-        f"Number of texts to embed: {1 if type(embeddings_input.input) == str else len(embeddings_input.input)}"
-    )
+    logging.info(f"Number of texts to embed: {len(embeddings_input.input)}")
     try:
-        embeddings = openai.Embedding.create(
-            input=embeddings_input.input, model=CONFIG["embeddings"]["model"]
-        )["data"]
+        embeddings = embedding_model.get_embeddings(
+            input=embeddings_input.input,
+            instruction=embeddings_input.instruction
+            if embeddings_input.instruction
+            else EMBEDDING_INSTRUCTION,
+        )
         return EmbeddingsResponse(data=embeddings)
-    except openai.error.APIError as e:
-        logging.error(f"OpenAI API returned an API Error: {e}")
+    except Exception as e:
+        logging.error(f"{e.__class__.__name__}: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"OpenAI API returned an API Error: {e}",
+            detail=f"{e.__class__.__name__}: {e}",
         )
 
 
@@ -58,7 +60,7 @@ async def get_completions(completions_input: CompletionsInput):
         if completions_input.info
         else PROMPT_NO_INFO(completions_input.query)
     )
-    logging.info("completions request:" + '\n' + prompt)
+    logging.info("completions request:" + "\n" + prompt)
     try:
         answer = openai.Completion.create(
             model=CONFIG["completions"]["model"],
@@ -71,10 +73,11 @@ async def get_completions(completions_input: CompletionsInput):
             presence_penalty=0.6
             # stop=['\n']
         )
-        logging.info("completions result:" + '\n' + pformat(answer))
+        logging.info(
+            f"completions result:\n{answer['choices'][0]['text'].lstrip()}\n\ntokens used: {answer['usage']['total_tokens']}"
+        )
         return CompletionsResponse(data=answer["choices"][0]["text"].lstrip())
     except Exception as e:
-    # except Exception as e:
         logging.error(f"{e.__class__.__name__}: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -90,5 +93,6 @@ if __name__ == "__main__":
             host=CONFIG["app"]["host"],
             port=int(CONFIG["app"]["port"]),
             log_level=CONFIG["app"]["log_level"],
+            root_path=CONFIG["app"]["root_path"],
         )
     )

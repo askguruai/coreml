67bb18fb2c40049cdbfa7f83cff48be5608fb660
Alexander Smirnov <furiousteabag@gmail.com>
2023-02-27 13:53:13 +0700

    simplify prompt

--- data/prompts.py
+++ data/prompts.py
@@ -3,16 +3,11 @@ EMBEDDING_INSTRUCTION = (
 )
 
 PROMPT_GENERAL = (
-    lambda info, query: f"""Using the following text, answer the following question. If the answer is not contained within the text, say that you are not able to answer because relevant information is not present.
+    lambda info, query: f"""Answer following question in detail based on given text. If no relevant information present, say \" no relevant info present, can't answer\".
 
-Text:
-\"\"\"
 {info}
-\"\"\"
 
-Question: {query}
-
-Answer:"""
+{query}"""
 )
 
 PROMPT_NO_INFO = (

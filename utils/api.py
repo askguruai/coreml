b1195f031d8ba858ca9813317dedaadd44325fb1
Alexander Smirnov <furiousteabag@gmail.com>
2023-02-28 14:43:31 +0700

    add error catching function

--- utils/api.py
+++ utils/api.py
@@ -1,40 +1,19 @@
-from typing import List
+import logging
+from functools import wraps
 
-from pydantic import BaseModel, Field
-
-
-class HTTPExceptionResponse(BaseModel):
-    detail: str = Field(example="OpenAI API returned an API Error: 400 Bad Request")
+from fastapi import HTTPException, status
 
 
-class EmbeddingsInput(BaseModel):
-    input: List[str] = Field(description="Texts to embed.", example=["Hello world!"])
-    instruction: str | None = Field(
-        description="Instruction to for embeddings model.",
-        example="Represent the knowledge base search query for retrieving relevant information.",
-    )
+def catch_errors(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except Exception as e:
+            logging.error(f"{e.__class__.__name__}: {e}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"{e.__class__.__name__}: {e}",
+            )
 
-
-class EmbeddingsResponse(BaseModel):
-    data: List[List[float]] = Field(
-        description="List of embeddings.",
-        example=[[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.10]],
-    )
-
-
-class CompletionsInput(BaseModel):
-    query: str = Field(
-        description="A query to get an asnwer to.", example="What did you do as a kid?"
-    )
-    info: str | None = Field(
-        default=None,
-        description="Information to use for answering the query. If not provided, the query will be answered without any context.",
-        example="When I was a kid I used to play drums",
-    )
-
-
-class CompletionsResponse(BaseModel):
-    data: str = Field(
-        description="Response to the given info and query.",
-        example="I used to play drums.",
-    )
+    return wrapper

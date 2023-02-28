823f1d622771f8e0ff5d832e1931ace51f93826a
Alexander Smirnov <furiousteabag@gmail.com>
2023-02-28 14:53:16 +0700

    add prompts versions

--- data/prompts.py
+++ data/prompts.py
@@ -1,19 +1,32 @@
-EMBEDDING_INSTRUCTION = (
-    "Represent the knowledge base search query for retrieving relevant information."
-)
+COMPLETIONS_PROMPT_OPENAI = (
+    lambda info, query: f"""Using the following text, answer the following question. If the answer is not contained within the text, say that you are not able to answer because relevant information is not present.
 
-PROMPT_GENERAL = (
-    lambda info, query: f"""Answer following question in detail based on given text. If no relevant information present, say \" no relevant info present, can't answer\".
-
+Text:
+\"\"\"
 {info}
+\"\"\"
 
-{query}"""
+Question: {query}
+
+Answer:"""
 )
 
-PROMPT_NO_INFO = (
+COMPLETIONS_PROMPT_OPENAI_NO_INFO = (
     lambda query: f"""You are helpful AI assistant created by Alex and Sergei which goal is to answer questions based on given information. You was asked question, answer it in detail.
 
 Question: {query}
 
 Answer:"""
+)
+
+EMBEDDING_INSTRUCTION = (
+    "Represent the knowledge base search query for retrieving relevant information."
+)
+
+COMPLETIONS_PROMPT_CUSTOM = (
+    lambda info, query: f"""Answer following question in detail based on given text. If no relevant information present, say \" no relevant info present, can't answer\".
+
+{info}
+
+{query}"""
 )

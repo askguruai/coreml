4794602b1bbf74667dced051fe65f28f101f5f95
Alexander Smirnov <furiousteabag@gmail.com>
2023-03-17 15:25:45 +0700

    comment out v2

--- main.py
+++ main.py
@@ -86,64 +86,64 @@ async def get_completions(completions_input: CompletionsInput):
 app.mount("/v1", v1)
 
 
-v2 = FastAPI()
+# v2 = FastAPI()
 
 
-@v2.get("/")
-async def docs_redirect():
-    return RedirectResponse(url=f"{CONFIG['app']['root_path']}/v2/docs")
+# @v2.get("/")
+# async def docs_redirect():
+#     return RedirectResponse(url=f"{CONFIG['app']['root_path']}/v2/docs")
 
 
-@app.on_event("startup")
-def init_models():
-    global embedding_model, completion_model
-    completion_model = CompletionModel(
-        model_name=CONFIG["v2.completions"]["model"],
-        device=CONFIG["v2.completions"]["device"],
-    )
-    embedding_model = EmbeddingModel(
-        model_name=CONFIG["v2.embeddings"]["model"], device=CONFIG["v2.embeddings"]["device"]
-    )
+# @app.on_event("startup")
+# def init_models():
+#     global embedding_model, completion_model
+#     completion_model = CompletionModel(
+#         model_name=CONFIG["v2.completions"]["model"],
+#         device=CONFIG["v2.completions"]["device"],
+#     )
+#     embedding_model = EmbeddingModel(
+#         model_name=CONFIG["v2.embeddings"]["model"], device=CONFIG["v2.embeddings"]["device"]
+#     )
 
 
-@v2.post(
-    "/embeddings/",
-    response_model=EmbeddingsResponse,
-    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
-)
-@catch_errors
-async def get_embeddings(embeddings_input: EmbeddingsInputInstruction):
-    logging.info(f"Number of texts to embed: {len(embeddings_input.input)}")
-    embeddings = embedding_model.get_embeddings(
-        input=embeddings_input.input,
-        instruction=embeddings_input.instruction
-        if embeddings_input.instruction
-        else EMBEDDING_INSTRUCTION,
-    )
-    return EmbeddingsResponse(data=embeddings)
+# @v2.post(
+#     "/embeddings/",
+#     response_model=EmbeddingsResponse,
+#     responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+# )
+# @catch_errors
+# async def get_embeddings(embeddings_input: EmbeddingsInputInstruction):
+#     logging.info(f"Number of texts to embed: {len(embeddings_input.input)}")
+#     embeddings = embedding_model.get_embeddings(
+#         input=embeddings_input.input,
+#         instruction=embeddings_input.instruction
+#         if embeddings_input.instruction
+#         else EMBEDDING_INSTRUCTION,
+#     )
+#     return EmbeddingsResponse(data=embeddings)
 
 
-@v2.post(
-    "/completions/",
-    response_model=CompletionsResponse,
-    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
-)
-@catch_errors
-async def get_completions(completions_input: CompletionsInput):
-    prompt = (
-        COMPLETIONS_PROMPT_CUSTOM(
-            completions_input.info.replace("\n\n", "\n"), completions_input.query
-        )
-        if completions_input.info
-        else COMPLETIONS_PROMPT_OPENAI_NO_INFO(completions_input.query)
-    )
-    logging.info("completions request:" + "\n" + prompt)
-    completion = completion_model.get_completion(prompt=prompt)
-    logging.info(f"completions result:\n{completion}")
-    return CompletionsResponse(data=completion)
+# @v2.post(
+#     "/completions/",
+#     response_model=CompletionsResponse,
+#     responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+# )
+# @catch_errors
+# async def get_completions(completions_input: CompletionsInput):
+#     prompt = (
+#         COMPLETIONS_PROMPT_CUSTOM(
+#             completions_input.info.replace("\n\n", "\n"), completions_input.query
+#         )
+#         if completions_input.info
+#         else COMPLETIONS_PROMPT_OPENAI_NO_INFO(completions_input.query)
+#     )
+#     logging.info("completions request:" + "\n" + prompt)
+#     completion = completion_model.get_completion(prompt=prompt)
+#     logging.info(f"completions result:\n{completion}")
+#     return CompletionsResponse(data=completion)
 
 
-app.mount("/v2", v2)
+# app.mount("/v2", v2)
 
 
 if __name__ == "__main__":

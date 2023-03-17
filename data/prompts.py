14853bd8fe61cc9d666330793de6082b98de2370
Alexander Smirnov <furiousteabag@gmail.com>
2023-03-17 15:45:17 +0700

    add system prompts

--- data/prompts.py
+++ data/prompts.py
@@ -1,3 +1,10 @@
+from collections import defaultdict
+
+COMPLETIONS_PROMPT_OPENAI_SYSTEM = {
+    "general": "You are helpful assistant which follows given instructions",
+    "support": "You are a customer support agent who is seeking to provide a complete, simple and helpful answer to a customer in a friendly manner.",
+}
+
 COMPLETIONS_PROMPT_OPENAI = (
     lambda info, query: f"""Using the following text, answer the following question. If the answer is not contained within the text, say that you are not able to answer because relevant information is not present.
 
@@ -23,9 +30,7 @@ Answer:"""
 #     "Represent the knowledge base search query for retrieving relevant information."
 # )
 
-EMBEDDING_INSTRUCTION = (
-    "Represent the text snippet for similarity search."
-)
+EMBEDDING_INSTRUCTION = "Represent the text snippet for similarity search."
 
 COMPLETIONS_PROMPT_CUSTOM = (
     lambda info, query: f"""Answer following question in detail based on given text.

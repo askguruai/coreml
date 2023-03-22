fd74e932b312b35aa1deb73a46aa5e6581afe322
Alexander Smirnov <furiousteabag@gmail.com>
2023-03-23 00:58:23 +0700

    add v3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from pprint import pformat
 from typing import List
 
@@ -6,15 +7,16 @@ import openai
 import uvicorn
 from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import RedirectResponse
+from torch import multiprocessing
 
 from data import (
-    COMPLETIONS_PROMPT_CUSTOM,
     COMPLETIONS_PROMPT_OPENAI,
     COMPLETIONS_PROMPT_OPENAI_NO_INFO,
     COMPLETIONS_PROMPT_OPENAI_SYSTEM,
     EMBEDDING_INSTRUCTION,
 )
-from ml import CompletionModel, EmbeddingModel
+from ml import EmbeddingModel
+from ml.completions import CompletionModel, OpenAICompletionModel, T5CompletionModel
 from utils import CONFIG
 from utils.api import catch_errors
 from utils.logging import run_uvicorn_loguru
@@ -28,6 +30,24 @@ from utils.schemas import (
 )
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def init_globals():
+    global manager
+    manager = multiprocessing.Manager()
+
+    global embedding_model, t5_completion_model, openai_completion_model
+    openai_completion_model = OpenAICompletionModel(
+        model_name=CONFIG["v1.completions"]["model"],
+    )
+    t5_completion_model = T5CompletionModel(
+        model_name=CONFIG["v2.completions"]["model"],
+        device=CONFIG["v2.completions"]["device"],
+    )
+    embedding_model = EmbeddingModel(
+        model_name=CONFIG["v2.embeddings"]["model"], device=CONFIG["v2.embeddings"]["device"]
+    )
 
 
 @app.get("/")
@@ -65,26 +85,7 @@ async def get_embeddings(embeddings_input: EmbeddingsInput):
 )
 @catch_errors
 async def get_completions(completions_input: CompletionsInput):
-    prompt = (
-        COMPLETIONS_PROMPT_OPENAI(completions_input.info, completions_input.query)
-        if completions_input.info
-        else COMPLETIONS_PROMPT_OPENAI_NO_INFO(completions_input.query)
-    )
-    logging.info("completions request:" + '\n' + prompt)
-    answer = openai.ChatCompletion.create(
-        model=CONFIG["v1.completions"]["model"],
-        messages=[
-            {
-                "role": "system",
-                "content": COMPLETIONS_PROMPT_OPENAI_SYSTEM[completions_input.mode],
-            },
-            {"role": "user", "content": prompt},
-        ],
-        temperature=0.9,
-        max_tokens=300,
-        presence_penalty=0.6,
-    )["choices"][0]["message"]["content"].lstrip()
-    logging.info("completions result:" + '\n' + answer)
+    answer = openai_completion_model.get_completion(completions_input=completions_input)
     return CompletionsResponse(data=answer)
 
 
@@ -97,18 +98,6 @@ v2 = FastAPI()
 @v2.get("/")
 async def docs_redirect():
     return RedirectResponse(url=f"{CONFIG['app']['root_path']}/v2/docs")
-
-
-@app.on_event("startup")
-def init_models():
-    global embedding_model, completion_model
-    completion_model = CompletionModel(
-        model_name=CONFIG["v2.completions"]["model"],
-        device=CONFIG["v2.completions"]["device"],
-    )
-    embedding_model = EmbeddingModel(
-        model_name=CONFIG["v2.embeddings"]["model"], device=CONFIG["v2.embeddings"]["device"]
-    )
 
 
 @v2.post(
@@ -135,23 +124,75 @@ async def get_embeddings(embeddings_input: EmbeddingsInputInstruction):
 )
 @catch_errors
 async def get_completions(completions_input: CompletionsInput):
-    prompt = (
-        COMPLETIONS_PROMPT_CUSTOM(
-            completions_input.info.replace("\n\n", "\n"), completions_input.query
-        )
-        if completions_input.info
-        else COMPLETIONS_PROMPT_OPENAI_NO_INFO(completions_input.query)
-    )
-    logging.info("completions request:" + "\n" + prompt)
-    completion = completion_model.get_completion(prompt=prompt)
-    logging.info(f"completions result:\n{completion}")
+    completion = t5_completion_model.get_completion(completions_input=completions_input)
     return CompletionsResponse(data=completion)
 
 
 app.mount("/v2", v2)
 
 
+v3 = FastAPI()
+
+
+@v3.get("/")
+async def docs_redirect():
+    return RedirectResponse(url=f"{CONFIG['app']['root_path']}/v3/docs")
+
+
+@v3.post(
+    "/completions/",
+    response_model=CompletionsResponse,
+    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPExceptionResponse}},
+)
+@catch_errors
+async def get_completions(completions_input: CompletionsInput):
+    logging.info("Received completions request")
+
+    state = manager.dict()
+    jobs = []
+    for completion_model in [openai_completion_model, t5_completion_model]:
+        job = multiprocessing.Process(
+            target=CompletionModel.get_completion_subprocess,
+            args=(completion_model, completions_input, state),
+        )
+        job.start()
+        jobs.append(job)
+
+    logging.info(f"Started {len(jobs)} jobs")
+
+    start = time.time()
+    while time.time() - start < int(CONFIG["v3.completions"]["general_timeout"]):
+        if openai_completion_model.__class__.__name__ in state or (
+            time.time() - start > int(CONFIG["v3.completions"]["openai_timeout"]) and state
+        ):
+            break
+        time.sleep(0.1)
+
+    logging.info(f"Finished in {round(time.time() - start, 2)} seconds with state {state}")
+
+    for job in jobs:
+        job.terminate()
+
+    if not state:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Timeout",
+        )
+
+    completion = (
+        state[openai_completion_model.__class__.__name__]
+        if openai_completion_model.__class__.__name__ in state
+        else state[t5_completion_model.__class__.__name__]
+    )
+
+    return CompletionsResponse(data=completion)
+
+
+app.mount("/v3", v3)
+
+
 if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
     run_uvicorn_loguru(
         uvicorn.Config(
             "main:app",

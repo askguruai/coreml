4b04ba4ea92d782f94629fba6b826ea86466b055
Alexander Smirnov <furiousteabag@gmail.com>
2023-08-01 05:59:43 +0300

    change summary prompt

--- ml/completions/openai_completion_model.py
+++ ml/completions/openai_completion_model.py
@@ -146,7 +146,7 @@ class OpenAICompletionModel(CompletionModel):
         self, summarization_input: SummarizationInput, api_version: ApiVersion
     ) -> CompletionsResponse:
         prompt = (
-            lambda text: f"Can you provide a comprehensive summary of the given text? The summary should cover all the key points and main ideas presented in the original text, while also condensing the information into a concise and easy-to-understand format. Please ensure that the summary includes relevant details and examples that support the main ideas, while avoiding any unnecessary information or repetition. The length of the summary should be appropriate for the length and complexity of the original text, providing a clear and accurate overview without omitting any important information.\n\nText:\n\"\"\"\n{text}\n\"\"\""
+            lambda text: f"Can you provide a comprehensive summary of the given article from a knowledge base? The summary should cover all the key points and main ideas presented in the original text, while also condensing the information into a concise and easy-to-understand format. Please ensure that the summary includes relevant details and examples that support the main ideas, while avoiding any unnecessary information or repetition. The length of the summary should be appropriate for the length and complexity of the original text, providing a clear and accurate overview without omitting any important information.\n\nText:\n\"\"\"\n{text}\n\"\"\""
         )
         max_request_size = 15_000
         args = {

96a56a0c81ec50d7dd3f63c21b77f03db13c3a97
Alexander Smirnov <furiousteabag@gmail.com>
2023-08-28 15:15:07 -0500

    add urls to output if they exist

--- ml/completions/openai_completion_model.py
+++ ml/completions/openai_completion_model.py
@@ -90,6 +90,7 @@ class OpenAICompletionModel(CompletionModel):
                     # "content": f"You are given the parts of the documents from a knowledge base and a question, compile a final answer.\nIn your answer, use only provided parts of the document.\nIf you don't know the answer, just say that you were unable to find an answer in the knowledge base.\nDon't try to make up an answer.\n\n" + (f"Each document contains `doc_id` and `doc_collection`. When you say something, refer to the document you are using in a format `{{'doc_id': '<id of the document>', 'doc_collection': '<collection of the document>'}}`.\n" if api_version == ApiVersion.v2 else f"")  + f"Parts of the documents:\n\"\"\"\n{completions_input.info}\n\"\"\"",
                 }
             )
+            messages.append({"role": "user", "content": f"Use URLs to image files from document in your answer if needed in a markdown format, like [](https://example.com/image.png)."})
         else:
             messages.append(
                 {
@@ -122,7 +123,7 @@ class OpenAICompletionModel(CompletionModel):
             else CONFIG["v2.completions"]["model"],
             "messages": messages,
             "temperature": 0.4,
-            "max_tokens": 500,
+            "max_tokens": 600,
             "stream": completions_input.stream,
         }
         if not completions_input.stream:

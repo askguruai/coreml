b1d3f33d7d0263460302d838d74b73ff6ecc6f23
Alexander Smirnov <furiousteabag@gmail.com>
2023-08-29 22:21:01 +0000

    apply formatting

--- utils/schemas.py
+++ utils/schemas.py
@@ -100,10 +100,7 @@ class CompletionsInput(BaseModel):
         default=False,
         description="If true, the response will be streamed as it is generated. This is useful for long-running requests.",
     )
-    include_image_urls: bool | None = Field(
-        default=False,
-        description="If include image urls in the output answer"
-    )
+    include_image_urls: bool | None = Field(default=False, description="If include image urls in the output answer")
 
     def __hash__(self):
         # to fasten the hash of chat history, only the first and last message are considered
